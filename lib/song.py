a2319e9e92af88fbf2ff696a6a52d3d1d3bddbf0
gilbert-ku <gilbert45ku@gmail.com>
2023-09-01 09:42:01 +0300

    50% complete

--- lib/song.py
+++ lib/song.py
@@ -1,7 +1,53 @@
 from config import CONN, CURSOR
 
 
+# class Song:
+
+#     def __init__(self, name, album):
+#         self.id = None
+#         self.name = name
+#         self.album = album
+
+#     @classmethod
+#     def create_table(self):
+#         sql = """
+#             CREATE TABLE IF NOT EXISTS songs (
+#                 id INTEGER PRIMARY KEY,
+#                 name TEXT,
+#                 album TEXT
+#             )
+#         """
+
+#         CURSOR.execute(sql)
+
+#     def save(self):
+#         sql = """
+#             INSERT INTO songs (name, album)
+#             VALUES (?, ?)
+#         """
+
+#         CURSOR.execute(sql, (self.name, self.album))
+#     @classmethod
+#     def create(cls, name, album):
+#         song = Song(name, album)
+#         song.save()
+#         return song
+    
+
+# song = Song.create("Hello", "25")
+# song.name
+# # => "Hello"
+# song.album
+# # => "25"
+
+import sqlite3
+
+CONN = sqlite3.connect('music.db')
+CURSOR = CONN.cursor()
+
 class Song:
+
+    all = []
 
     def __init__(self, name, album):
         self.id = None
@@ -9,13 +55,21 @@ class Song:
         self.album = album
 
     @classmethod
-    def create_table(self):
+    def create_table(cls):
         sql = """
             CREATE TABLE IF NOT EXISTS songs (
                 id INTEGER PRIMARY KEY,
                 name TEXT,
                 album TEXT
             )
+        """
+
+        CURSOR.execute(sql)
+
+    @classmethod
+    def drop_table(cls):
+        sql = """
+            DROP TABLE IF EXISTS songs
         """
 
         CURSOR.execute(sql)
@@ -27,15 +81,11 @@ class Song:
         """
 
         CURSOR.execute(sql, (self.name, self.album))
+
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
+
     @classmethod
     def create(cls, name, album):
         song = Song(name, album)
         song.save()
         return song
-    
-
-song = Song.create("Hello", "25")
-song.name
-# => "Hello"
-song.album
-# => "25"
